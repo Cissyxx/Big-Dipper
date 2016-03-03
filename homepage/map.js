e1626a0c123e06cc6a3803aa04a84821377878ab
amyzheng1995 <amyzheng1995@gmail.com>
2016-03-03 03:47:19 -0500

    able to display current location address after clicking a button

--- homepage/map.js
+++ homepage/map.js
@@ -1,29 +1,32 @@
-
+var pos;
+var map;
+var geocoder;
+// var infowindow = new google.maps.InfoWindow();
+var infowindow;
+var marker;
 function initialize() {
   var mapOptions = {
    center: new google.maps.LatLng(42.730787,-73.682488),
    zoom: 6,
    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var x = document.getElementById("location");
-  var map = new google.maps.Map(document.getElementById("googleMap"),
+  
+  map = new google.maps.Map(document.getElementById("googleMap"),
             mapOptions);
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
   if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
+            pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
             map.setCenter(pos);
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
             x.innerHTML="Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
-            x.innerHTML = "Geolocation is not supported by this browser.";
+            
           });
         } 
     else {
@@ -37,9 +40,37 @@ infoWindow.setContent(browserHasGeolocation ?
                       'Error: The Geolocation service failed.' :
                       'Error: Your browser doesn\'t support geolocation.');
 }
-// google.maps.event.addDomListener(window, 'load', initialize);
 
+function codeLatLng(){
+    var latlng = new google.maps.LatLng(pos.lat, pos.lng);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': latlng}, function(results, status)
+    {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
+            if (results[0])
+            {
+                map.setZoom(11);
+                marker = new google.maps.Marker(
+                {
+                    position: latlng,
+                    map: map
+                });
+                infowindow = new google.maps.InfoWindow({map: map});
+                infowindow.setContent("Address: "+ results[0].formatted_address);
+                infowindow.open(map, marker);
 
-            
+            }
+            else
+            {
+                alert("No results found");
+            }
+        }
+        else
+        {
+            alert("Geocoder failed due to: " + status);
+        }
+    });
+}
 
      
