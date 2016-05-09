b40c34be45564237b47f734b9603ff568c8219b1
Cissyxx <cissyxx@gmail.com>
2016-05-09 19:26:40 -0400

    Added more tests

--- src/com/dipper/big/test/MapManagerTest.java
+++ src/com/dipper/big/test/MapManagerTest.java
@@ -2,61 +2,30 @@ package com.dipper.big.test;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import com.dipper.big.Path;
 import com.dipper.big.LocationException;
 import com.dipper.big.MapManager;
-
+import com.dipper.big.Path;
 
 public class MapManagerTest
 {
-
-    private MapManager mManager;
-
-    //    @Test
-    //    public void test1() throws Exception  {
-    //        //  create mock
-    //        final DistanceMatrix distanceMatrix = Mockito.mock(DistanceMatrix.class);
-    //        System.out.println("1");
-    //        final GeoApiContext mContext = Mockito.mock(GeoApiContext.class);
-    //        System.out.println("2");
-    //        final String apiKey = "123456";
-    //        System.out.println("3");
-    //
-    //        Mockito.doReturn(mContext).when(mContext.setApiKey(apiKey));
-    //        //        Mockito.when(mManager.mContext.setApiKey(apiKey)).
-    //        //
-    //        //        // define return value for method getUniqueId()
-    //        //        Mockito.when(DistanceMatrixApi.getDistanceMatrix(mContext, mArray,mArray).await()).thenReturn(distanceMatrix);
-    //        //
-    //        //        // use mock in test....
-    //        //        assertEquals(DistanceMatrixApi.getDistanceMatrix(mContext, mArray,mArray).await(), distanceMatrix);
-    //    }
+    private static String API_KEY = "AIzaSyALZvDqmZKte0ru1-fekJQE9ekCgovQcYw";
+    private static MapManager mManager;
 
     @Test
-    public void test_getAllOptimalPath() throws LocationException
-    {
-        final Path mResult = new Path();
+    public void test_getAllOptimalPath() throws LocationException{
+        //Initialization
+        mManager = MapManager.getInstance(API_KEY);
+
         final Path destinations = new Path();
-        Mockito.doReturn(mResult).when(mManager.getAllOptimalPath(destinations, true));
-        Assert.assertEquals(mResult, mResult);
+        destinations.add("central park");
+        destinations.add("high line");
+        destinations.add("times square");
+
+        final String expectedOutput = "[Central Park, New York, NY 10024, USA, Theater District, New York, NY, USA, The High Line, New York, NY 10011, USA]";
+
+        //Grab optimal path
+        Assert.assertEquals(mManager.getAllOptimalPath(destinations, false).getPath().toString(), expectedOutput);
     }
-    // public DistanceResult getAllOptimalPath(DistanceResult mDestinations, boolean isStartLocSet)
 
-    //    public static MapManager getInstance(final String API_KEY)
-    //    {
-    //        mManager.mContext.setApiKey(API_KEY);
-    //        return mManager;
-    //    }
-
-    //    // Demonstrates the return of multiple values
-    //    @Test
-    //    public void testMoreThanOneReturnValue()  {
-    //        final Iterator i= mock(Iterator.class);
-    //        when(i.next()).thenReturn("Mockito").thenReturn("rocks");
-    //        final String result=i.next()+" "+i.next();
-    //        //assert
-    //        assertEquals("Mockito rocks", result);
-    //    }
 }
